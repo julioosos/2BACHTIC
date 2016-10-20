2eb13ecc6e71d79b5db1fd15cdd5f8f4fe7785de
YellowZaki <algoca99@gmail.com>
2016-10-20 13:25:35 +0200

    1

---
+++ script.js
@@ -0,0 +1,26 @@
+
+function Limpiar(comboBox) {
+    while (comboBox.options.length > 0) {                
+        comboBox.remove(0);
+    }        
+}
+
+function Borrar(x) {
+	x.remove(x.selectedIndex);        
+}
+
+
+
+
+
+
+function Anadir(idlista,idvalor) {
+    var option = document.createElement("option");
+    option.text = document.getElementById(idvalor).value;
+    document.getElementById(idlista).add(option);
+}
+
+
+function Procesar(idtexto) {
+    document.getElementById(idtexto).value = "Hola";
+}
