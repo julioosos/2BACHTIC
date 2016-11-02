b65f883b10590c03ec351883e91e2866cc2d551e
usuario <miguel200902@hotmail.com>
2016-11-02 12:22:57 +0100

    Tarea #12

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-
+function stopRKey(evt) {
+   var evt = (evt) ? evt : ((event) ? event : null);
+   var node = (evt.target) ? evt.target : ((evt.srcElement) ? evt.srcElement : null);
+   if ((evt.keyCode == 13) && (node.type=="text")) {return false;}
+}
 function Limpiar(idlista) {
     var opcion = document.getElementById(idlista)
     while (opcion.options.length > 0) {                
